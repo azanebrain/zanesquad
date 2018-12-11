7588736fa8f64deea22cf284a8aa4b23e990fc18
AJ Zane <androiddreams@ajzane.com>
2018-12-11 09:01:32 -0800

    Screen Wrapper: Adds basic screen wrapper
    - Adds Toolbar Module
    - Adds Landing Screen Module

---
+++ src/app/toolbar/toolbar.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { ToolbarComponent } from './toolbar.component';
+
+@NgModule({
+  declarations: [
+    ToolbarComponent,
+  ],
+  exports: [
+    ToolbarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+  ]
+})
+export class ToolbarModule { }
