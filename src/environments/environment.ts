a210f73ea2f55d9962203a9e82710dbf1577c32b
AJ Zane <androiddreams@ajzane.com>
2018-12-18 08:50:59 -0800

    Environment: Adds environment interface

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
+import { Environment } from './environment.model';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const environment: Environment = {
+  domain: 'localhost:3000/api',
+  production: false,
 };
 
 /*
