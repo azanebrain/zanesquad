0228151c05159b014833ea3b4127d23944156e36
AJ Zane <androiddreams@ajzane.com>
2019-01-19 09:15:18 -0800

    Companies Seeds: Adds URLs to seed data

--- Seeds/companies.sql
+++ Seeds/companies.sql
@@ -2,17 +2,22 @@
 
 insert into "companies" (Guid, Name) values
 ('34b8f278-ada1-4016-8c63-348f6cf5f6c1',
-  'Uber'
+  'Uber',
+  'https://airbnb.com'
 ),
 ('a45127f9-dcac-4f07-9ad4-72437a099d17',
-  'AirBnB'
+  'AirBnB',
+  'https://siteground.com'
 ),
 ('1790d851-29be-4a73-a0e1-8913e0637386',
-  'SiteGround'
+  'SiteGround',
+  'https://www.bird.co'
 ),
 ('ca9e7350-134d-4ea7-824f-875412a94484',
-  'Bird Transportation'
+  'Bird Transportation',
+  'https://robinhood.com'
 ),
 ('40469d10-592d-4f04-8493-e29373492be4',
-  'Robinhood'
+  'Robinhood',
+  'https://uber.com'
 );
